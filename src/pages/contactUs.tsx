ac1f02641a1433e413210f36f32929b211e57132
Lachlan MacPhee <lachlan.macphee@mantelgroup.com.au>
2023-11-15 00:53:38 +1100

    contact us skeleton

--- src/pages/contactUs.tsx
+++ src/pages/contactUs.tsx
@@ -1,3 +1,181 @@
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  firstName: z.string().min(2),
+  lastName: z.string().min(2),
+  emailAddress: z.string().email(),
+  phoneNumber: z.string().min(10),
+  subject: z.enum([
+    "general",
+    "sponsor",
+    "fundraising",
+    "advertising",
+    "runningTip",
+  ]),
+  comments: z
+    .string()
+    .min(10, {
+      message: "Comment must be at least 10 characters.",
+    })
+    .max(160, {
+      message: "Comment must not be longer than 30 characters.",
+    }),
+});
+
 export const ContactUs = () => {
-  return <div className="flex items-center flex-col">Contact Us</div>;
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      emailAddress: "",
+      phoneNumber: "",
+      subject: "general",
+      comments: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+    // send email
+  }
+
+  return (
+    <div className="flex flex-col gap-8">
+      <h1 className="text-5xl font-bold">Contact Us</h1>
+
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+          <FormField
+            control={form.control}
+            name="firstName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>First Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="John" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="lastName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Last Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="Smith" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="emailAddress"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email Address</FormLabel>
+                <FormControl>
+                  <Input placeholder="john.smith@gmail.com" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="phoneNumber"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Phone Number</FormLabel>
+                <FormControl>
+                  <Input placeholder="0412345678" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="subject"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Subject</FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectItem value="general">General Enquiry</SelectItem>
+                    <SelectItem value="sponsor">Sponsor Enquiry</SelectItem>
+                    <SelectItem value="fundraising">
+                      Fundraising Enquiry
+                    </SelectItem>
+                    <SelectItem value="advertising">
+                      Advertising Enquiry
+                    </SelectItem>
+                    <SelectItem value="runningTip">Running Tip</SelectItem>
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="comments"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Comments</FormLabel>
+                <FormControl>
+                  <Textarea
+                    placeholder="Write your comment here"
+                    className="resize-none"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </div>
+  );
 };
