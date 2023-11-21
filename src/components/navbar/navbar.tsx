ccaea1af02c7b0d71a6db34b544269329c59a9af
Lachlan MacPhee <lachlan.macphee@mantelgroup.com.au>
2023-11-21 23:34:58 +1100

    hide theme on mobile

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -5,20 +5,20 @@ import { DesktopNavMenu } from "./desktopNavMenu";
 import { MobileNavMenu } from "./mobileNavMenu";
 import { AvatarIconModal } from "./avatar";
 import { ModeToggle } from "./mode-toggle";
-
-// Icons
 import { LogInOut } from "./logInOut";
 
 export function Navbar() {
   return (
     <div className="flex justify-between py-4 px-8 items-center">
-      <Link to="/" className="normal-case w-[190px] font-bold text-2xl">
+      <Link to="/" className="normal-case font-bold text-2xl">
         Gunn Runners
       </Link>
       <DesktopNavMenu />
-      <div className="flex gap-3 justify-end w-[190px]">
+      <div className="flex gap-3 justify-end">
         <AvatarIconModal />
-        <ModeToggle />
+        <span className="hidden xl:block">
+          <ModeToggle />
+        </span>
         <LogInOut />
         <MobileNavMenu />
       </div>
