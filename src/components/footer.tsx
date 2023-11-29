da8fc12dd6282721700696cc212a23e55a35f5ae
Lachlan MacPhee <lachlan.macphee@mantelgroup.com.au>
2023-11-29 15:24:56 +1100

    update social links

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,15 +3,15 @@ import { FaMeetup } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { buttonVariants } from "./ui/button";
 
-const Links = () => (
+const SocialLinks = () => (
   <>
-    <Link to="">
+    <Link to="https://www.facebook.com/groups/1147699358622893/">
       <Facebook className="w-8 h-8" />
     </Link>
-    <Link to="">
+    <Link to="https://www.instagram.com/gunn.runners/">
       <Instagram className="w-8 h-8" />
     </Link>
-    <Link to="">
+    <Link to="https://www.meetup.com/gunnrunners/">
       <FaMeetup className="w-8 h-8" />
     </Link>
   </>
@@ -21,11 +21,11 @@ export function Footer() {
   return (
     <footer className="gap-4 mb-4 mx-8 flex flex-wrap justify-between items-center">
       <div className="flex sm:hidden w-full justify-center gap-4">
-        <Links />
+        <SocialLinks />
       </div>
       <p className="font-bold">&copy; Gunn Runners</p>
       <div className="hidden sm:flex justify-center gap-4">
-        <Links />
+        <SocialLinks />
       </div>
       <Link to="/privacy" className={buttonVariants({ variant: "link" })}>
         Privacy Policy
