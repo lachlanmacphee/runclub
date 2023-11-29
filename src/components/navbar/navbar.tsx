158055e9266b36a51c2aa0bf1bb09a7cfab272d3
Lachlan MacPhee <lachlan.macphee@mantelgroup.com.au>
2023-11-30 00:32:46 +1100

    update logo

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -10,11 +10,17 @@ import { LogInOut } from "./logInOut";
 export function Navbar() {
   return (
     <div className="flex justify-between py-4 px-8 items-center">
-      <Link to="/" className="normal-case font-bold text-2xl">
-        Gunn Runners
+      <Link to="/" className="normal-case font-bold text-2xl w-[250px]">
+        <div className="flex gap-4 items-center">
+          <img
+            className="w-10 h-10 rounded-full"
+            src="/src/assets/gunnieslogo.png"
+          />
+          <span>Gunn Runners</span>
+        </div>
       </Link>
       <DesktopNavMenu />
-      <div className="flex gap-3 justify-end">
+      <div className="flex gap-3 justify-end w-[250px]">
         <AvatarIconModal />
         <span className="hidden xl:block">
           <ModeToggle />
