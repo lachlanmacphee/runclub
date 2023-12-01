fb721f645108f4481942b3f70f7c87106d9f12f2
Lachlan MacPhee <github@lachlanmacphee.com>
2023-12-02 10:41:54 +1100

    dynamic location images

--- models/event.go
+++ models/event.go
@@ -17,16 +17,27 @@ type Event struct {
 
 func (e Event) FormatLocation(location string) string {
 	switch location {
-	case "albertParkLake":
-		return "Albert Park Lake"
-	case "southMelbourneBeach":
-		return "South Melbourne Beach"
-	default:
-		fmt.Println("Something went wrong while converting location.")
-		return "N/A"
+		case "albertParkLake":
+			return "Albert Park Lake"
+		case "southMelbourneBeach":
+			return "South Melbourne Beach"
+		default:
+			return "Invalid Location"
 	}
 }
 
 func (e Event) FormatDate(date time.Time) string {
 	return date.Format("Monday, January 2, 2006")
+}
+
+func (e Event) GetLocationImage(location string) string {
+	switch location {
+		case "albertParkLake":
+			return "https://www.onlymelbourne.com.au/image/ico-albertpark.jpg"
+		case "southMelbourneBeach":
+			return "https://elslighting.com.au/wp-content/uploads/2020/05/SMLSC_3-1024x767.jpg"
+		default:
+			fmt.Println("Something went wrong while obtaining the location image.")
+			return "N/A"
+	}
 }
