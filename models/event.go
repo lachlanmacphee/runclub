6d4df1a6289d321383a47eddc9c9212db7d8ba00
Lachlan MacPhee <github@lachlanmacphee.com>
2023-12-02 10:22:22 +1100

    format dates

--- models/event.go
+++ models/event.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"fmt"
 	"time"
 
 	"gorm.io/gorm"
@@ -12,4 +13,20 @@ type Event struct {
 	Location string
 	IsComplete bool
 	Participants []Participant
+}
+
+func (e Event) FormatLocation(location string) string {
+	switch location {
+	case "albertParkLake":
+		return "Albert Park Lake"
+	case "southMelbourneBeach":
+		return "South Melbourne Beach"
+	default:
+		fmt.Println("Something went wrong while converting location.")
+		return "N/A"
+	}
+}
+
+func (e Event) FormatDate(date time.Time) string {
+	return date.Format("Monday, January 2, 2006")
 }
