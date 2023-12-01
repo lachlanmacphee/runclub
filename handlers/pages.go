bcd436a3405afaee26345f1b4ad373447fc0e534
Lachlan MacPhee <github@lachlanmacphee.com>
2023-12-01 17:14:57 +1100

    refactor pages and layout

--- handlers/pages.go
+++ handlers/pages.go
@@ -5,17 +5,17 @@ import (
 )
 
 func Home(c *fiber.Ctx) error {
-	return c.Render("home", nil, "layouts/main")
+	return c.Render("pages/home/index", nil, "layout/main")
 }
 
 func Event(c *fiber.Ctx) error {
-	return c.Render("events", nil, "layouts/main")
+	return c.Render("pages/events/index", nil, "layout/main")
 }
 
 func Faq(c *fiber.Ctx) error {
-	return c.Render("faq", nil, "layouts/main")
+	return c.Render("pages/faq/index", nil, "layout/main")
 }
 
 func Contact(c *fiber.Ctx) error {
-	return c.Render("contact", nil, "layouts/main")
+	return c.Render("pages/contact/index", nil, "layout/main")
 }
