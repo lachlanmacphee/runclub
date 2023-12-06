9df20e3ee291f077f91b94069595e60ab9564de6
Lachlan MacPhee <lachlan.macphee@mantelgroup.com.au>
2023-12-06 22:25:15 +1100

    club name form

--- src/pages/manageClub.tsx
+++ src/pages/manageClub.tsx
@@ -1,3 +1,9 @@
+import { ClubNameForm } from "@/components/manage/clubNameForm";
+
 export function ManageClub() {
-  return <div>Manage Club</div>;
+  return (
+    <div className="flex flex-col gap-8">
+      <ClubNameForm />
+    </div>
+  );
 }
