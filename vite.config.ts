c20415c0884b103311fcc870e1f8d1441e917c1c
Lachlan MacPhee <lachlan.macphee@mantelgroup.com.au>
2023-12-07 11:47:15 +1100

    add test pwa functionality

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
