d67faf98599d6224317859dec138ccbb7bccf60e
Lachlan MacPhee <lachlan.macphee@mantelgroup.com.au>
2023-12-07 12:00:34 +1100

    further work towards pwa

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,30 @@ import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      manifest: {
+        name: "Gunn Runners App",
+        short_name: "GunniesApp",
+        description: "Manage timing and operations for Gunn Runners",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
