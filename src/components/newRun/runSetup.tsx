1601174721ba4e8e7ccdc93afcf8a9f1a87a4332
Lachlan MacPhee <github@lachlanmacphee.com>
2024-01-11 11:31:34 +1100

    rework run setup for better mobile experience

--- src/components/newRun/runSetup.tsx
+++ src/components/newRun/runSetup.tsx
@@ -11,7 +11,6 @@ import { distanceOptions } from "@/lib/constants";
 import CreatableSelect from "react-select/creatable";
 import Select from "react-select";
 import { Button } from "@/components/ui/button";
-import { Switch } from "@/components/ui/switch";
 import { toast } from "@/components/ui/use-toast";
 import { DatePicker } from "@/components/ui/date-picker";
 import {
@@ -33,9 +32,11 @@ import {
 import { Input } from "@/components/ui/input";
 
 // Icons
-import { ArrowDown10, ArrowUp10, Plus, Trash } from "lucide-react";
+import { ArrowDown10, ArrowUp10, Trash } from "lucide-react";
 import { usePastParticipants } from "@/hooks/usePastParticipants";
 import { RunSetupConfirmationAlert } from "./runSetupConfirmationAlert";
+import { Label } from "../ui/label";
+import { Switch } from "../ui/switch";
 
 const FormSchema = z
   .object({
@@ -79,6 +80,15 @@ export function RunSetup({
   const [order, setOrder] = useState<number>(-1);
   const [isConfirmationModalOpen, setIsConfirmationModalOpen] = useState(false);
   const { pb } = usePocket();
+  const [newParticipantBib, setNewParticipantBib] = useState<string>("100");
+  const [newParticipantName, setNewParticipantName] = useState<{
+    value: string;
+    label: string;
+  } | null>();
+  const [newParticipantDistance, setNewParticipantDistance] = useState<{
+    value: string;
+    label: string;
+  } | null>(distanceOptions[1]);
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
     mode: "onBlur",
@@ -86,7 +96,7 @@ export function RunSetup({
       date: initialDate,
       location: "albertParkLake",
       // @ts-ignore
-      participants: [{ bib: "100", name: null, distance: distanceOptions[1] }],
+      participants: [],
     },
   });
 
@@ -148,20 +158,29 @@ export function RunSetup({
     (participant) => participant.name?.label
   );
   const participantsLength = participants.length;
-  const newBibNumber = Number(participants[participantsLength - 1].bib) + order;
 
-  function addParticipant() {
+  function addParticipant(
+    bib: string,
+    name: { value: string; label: string },
+    distance: { value: string; label: string }
+  ) {
     append(
       {
-        bib: String(newBibNumber),
-        // @ts-ignore
-        name: null,
-        distance: distanceOptions[1],
+        bib,
+        name,
+        distance,
       },
       {
         focusName: `participants.${participantsLength}.name`,
       }
     );
+    setNewParticipantBib(
+      order == 1
+        ? String(parseInt(newParticipantBib) + 1)
+        : String(parseInt(newParticipantBib) - 1)
+    );
+    setNewParticipantName(null);
+    setNewParticipantDistance(distanceOptions[1]);
   }
 
   return (
@@ -222,7 +241,79 @@ export function RunSetup({
               </FormItem>
             )}
           />
-          <div></div>
+          <div className="space-y-2">
+            <div>
+              <Label>Bib</Label>
+              <Input
+                value={newParticipantBib}
+                onChange={(e) => setNewParticipantBib(e.target.value)}
+              />
+            </div>
+            <div>
+              <Label>Name</Label>
+              <CreatableSelect
+                className="custom-react-select-container"
+                classNamePrefix="custom-react-select"
+                placeholder="Select..."
+                value={newParticipantName}
+                onChange={(newName) => setNewParticipantName(newName)}
+                options={pastParticipants
+                  .filter(
+                    (participant) =>
+                      !participantNames.includes(participant.name)
+                  )
+                  .map((participant) => {
+                    return {
+                      label: participant.name,
+                      value: participant.user_id
+                        ? participant.user_id
+                        : participant.name,
+                    };
+                  })}
+              />
+            </div>
+            <div>
+              <Label>Distance</Label>
+              <Select
+                className="custom-react-select-container"
+                classNamePrefix="custom-react-select"
+                placeholder="Select..."
+                value={newParticipantDistance}
+                onChange={(newDistance) =>
+                  setNewParticipantDistance(newDistance)
+                }
+                options={distanceOptions}
+              />
+            </div>
+            <div className="flex justify-between">
+              <div className="flex items-center space-x-2">
+                <ArrowDown10 />
+                <Switch
+                  id="order"
+                  onCheckedChange={(val) => setOrder(val ? 1 : -1)}
+                />
+                <ArrowUp10 />
+              </div>
+              <Button
+                type="button"
+                onClick={() => {
+                  if (
+                    newParticipantBib &&
+                    newParticipantName &&
+                    newParticipantDistance
+                  ) {
+                    addParticipant(
+                      newParticipantBib,
+                      newParticipantName,
+                      newParticipantDistance
+                    );
+                  }
+                }}
+              >
+                Add Participant
+              </Button>
+            </div>
+          </div>
           <div className="flex flex-col gap-2">
             <div className="grid grid-cols-[65px_2fr_1fr_40px] gap-x-2">
               <p>Bib</p>
@@ -307,34 +398,21 @@ export function RunSetup({
                 )}
               </div>
             ))}
-            <div className="grid grid-cols-[1fr_2fr_40px] gap-x-2">
-              <div className="flex items-center space-x-2">
-                <ArrowDown10 />
-                <Switch
-                  id="order"
-                  onCheckedChange={(val) => setOrder(val ? 1 : -1)}
-                />
-                <ArrowUp10 />
-              </div>
+            {participantsLength == 0 && (
+              <span className="text-center">No participants added yet.</span>
+            )}
+            {form.formState.errors.participants?.root && (
               <span className="font-bold text-destructive">
-                {form.formState.errors.participants?.root && (
-                  <p>{form.formState.errors.participants.root.message}</p>
-                )}
+                {form.formState.errors.participants.root.message}
               </span>
-              <div className="flex justify-center">
-                <Button
-                  type="button"
-                  variant="outline"
-                  size="icon"
-                  onClick={addParticipant}
-                >
-                  <Plus />
-                </Button>
-              </div>
-            </div>
+            )}
           </div>
           <div className="flex justify-center">
-            <Button type="submit" className="w-48">
+            <Button
+              type="submit"
+              disabled={participantsLength == 0}
+              className="w-48"
+            >
               Create Run
             </Button>
           </div>
