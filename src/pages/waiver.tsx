00255648d4f1ffcca9b66fbe37301da7f13e10dd
Lachlan MacPhee <github@lachlanmacphee.com>
2024-01-31 21:59:11 +1100

    modify validation messages

--- src/pages/waiver.tsx
+++ src/pages/waiver.tsx
@@ -22,30 +22,90 @@ import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Checkbox } from "@/components/ui/checkbox";
 
 const FormSchema = z.object({
-  email: z.string().email(),
-  fname: z.string().min(2),
-  lname: z.string().min(2),
-  suburb: z.string().min(3),
-  postcode: z.string().min(4),
-  phone: z.string().min(10).max(10),
-  gender: z.enum(["Male", "Female", "Other", "Blank"]),
-  age: z.enum([
-    "Under 18",
-    "18 - 30",
-    "31 - 40",
-    "41 - 50",
-    "Above 50",
-    "Blank",
-  ]),
-  satisfiesAgeReq: z.enum(["Over 18", "Have Permission"]),
+  email: z
+    .string({
+      errorMap: () => ({
+        message: "Email is required and must be the correct format.",
+      }),
+    })
+    .email(),
+  fname: z
+    .string({
+      errorMap: () => ({
+        message: "First name is required and must be 2 characters minimum.",
+      }),
+    })
+    .min(2),
+  lname: z
+    .string({
+      errorMap: () => ({
+        message: "Last name is required and must be 2 characters minimum.",
+      }),
+    })
+    .min(2),
+  suburb: z
+    .string({
+      errorMap: () => ({
+        message: "Suburb is required and must be 3 characters minimum.",
+      }),
+    })
+    .min(3),
+  postcode: z
+    .string({
+      errorMap: () => ({
+        message: "Postcode is required and must be 4 characters minimum.",
+      }),
+    })
+    .min(4),
+  phone: z
+    .string({
+      errorMap: () => ({
+        message: "Phone number is required and must be 10 characters.",
+      }),
+    })
+    .min(10)
+    .max(10),
+  gender: z.enum(["Male", "Female", "Other", "Blank"], {
+    errorMap: () => ({
+      message: "Gender is a required field.",
+    }),
+  }),
+  age: z.enum(
+    ["Under 18", "18 - 30", "31 - 40", "41 - 50", "Above 50", "Blank"],
+    {
+      errorMap: () => ({
+        message: "Age is a required field.",
+      }),
+    }
+  ),
+  satisfiesAgeReq: z.enum(["Over 18", "Have Permission"], {
+    errorMap: () => ({
+      message: "This is a required field.",
+    }),
+  }),
   readTermsAndConditions: z.literal<boolean>(true, {
     errorMap: () => ({
       message:
         "It's a requirement that you read and accept the terms and conditions.",
     }),
   }),
-  emergencyContactName: z.string(),
-  emergencyContactPhone: z.string().min(10).max(10),
+  emergencyContactName: z
+    .string({
+      errorMap: () => ({
+        message:
+          "Emergency contact name is a required field and must be at least 2 characters.",
+      }),
+    })
+    .min(2),
+  emergencyContactPhone: z
+    .string({
+      errorMap: () => ({
+        message:
+          "Emergency contact phone is a required field and must be 10 characters.",
+      }),
+    })
+    .min(10)
+    .max(10),
   referredBy: z.string().optional(),
 });
 
@@ -55,6 +115,7 @@ export const Waiver = () => {
 
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
+    reValidateMode: "onBlur",
     defaultValues: {
       email: "",
       fname: "",
