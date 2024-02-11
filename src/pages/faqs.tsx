22ca460054d3c46fe07cfbf38e51192ccf9585d2
Lachlan MacPhee <github@lachlanmacphee.com>
2024-02-11 13:57:55 +1100

    add past results to FAQ

--- src/pages/faqs.tsx
+++ src/pages/faqs.tsx
@@ -13,28 +13,46 @@ export function FAQs() {
             </div>
           ))}
         </div>
-        <h2 className="text-xl font-bold">How do I use this site?</h2>
-        <iframe
-          height="315"
-          src="https://www.youtube.com/embed/AnM3uq0jtWY?si=5F86eFHo3f-_PiJL"
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowFullScreen
-        ></iframe>
-        <iframe
-          height="315"
-          src="https://www.youtube.com/embed/_u9m-tyygD0?si=_7fhsYgvdMV6RIDZ"
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowFullScreen
-        ></iframe>
-        <iframe
-          height="315"
-          src="https://www.youtube.com/embed/EAGqJjKvuq4?si=YR3UG8CBof3-znRg"
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowFullScreen
-        ></iframe>
+        <div className="flex flex-col gap-2">
+          <h2 className="text-lg font-semibold">
+            Where can I access the results from the previous website?
+          </h2>
+          <p>
+            We provide a zip archive of the previous results from the last Gunn
+            Runners website.{" "}
+            <a
+              href="/pastResults.zip"
+              className="text-blue-600 dark:text-blue-500 hover:underline"
+            >
+              Click here
+            </a>{" "}
+            to download.
+          </p>
+        </div>
+        <div className="flex flex-col gap-2">
+          <h2 className="text-xl font-bold">How do I use this site?</h2>
+          <iframe
+            height="315"
+            src="https://www.youtube.com/embed/AnM3uq0jtWY?si=5F86eFHo3f-_PiJL"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
+          <iframe
+            height="315"
+            src="https://www.youtube.com/embed/_u9m-tyygD0?si=_7fhsYgvdMV6RIDZ"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
+          <iframe
+            height="315"
+            src="https://www.youtube.com/embed/EAGqJjKvuq4?si=YR3UG8CBof3-znRg"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
+        </div>
       </div>
     </div>
   );
